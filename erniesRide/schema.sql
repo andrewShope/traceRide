00aaaba8ca3ae446afbc110b2ffa8b708e81dc10
Andrew Shope <andrew.shope88@gmail.com>
2018-04-11 16:28:05 +0000

    changed main bike picture

--- erniesRide/schema.sql
+++ erniesRide/schema.sql
@@ -2,5 +2,9 @@ drop table if exists entries;
 create table entries (
 	id integer primary key autoincrement,
 	email text not null,
+	firstName text not null,
+	lastName text not null,
+	city text not null,
+	state text not null,
 	pledge numeric (10, 2) not null
 );
